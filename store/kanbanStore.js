aff2add18f5011ee9ffb5a1dab354302b58226ae
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-04-07 14:51:18 -0300

    #2-release: componentizando cardlist

--- store/kanbanStore.js
+++ store/kanbanStore.js
@@ -4,26 +4,8 @@ export const useKanbanStore = defineStore('kanban', {
     state: () => ({
         items: 0,
         kList: {
-            trainTable: [
-                {name:"vue.draggable", order:1},
-                {name: "draggable", order:2},
-                {name: "component", order: 3},
-                {name: "for", order: 4},
-                {name: "vue.js 2.0", order: 5},
-                {name: "based", order: 6},
-                {name: "on", order: 7},
-                {name: "Sortablejs", order: 8}
-              ], // cards rodando rotinas de treino
-            testTable: [
-              {name:"vue.draggable", order:9},
-              {name: "draggable", order:10},
-              {name: "component", order: 11},
-              {name: "for", order: 12},
-              {name: "vue.js 2.0", order: 13},
-              {name: "based", order: 14},
-              {name: "on", order: 15},
-              {name: "Sortablejs", order: 16}
-              ], // cards rodando na nossa API, que permitam passar parametros
+            trainTable: [], // cards rodando rotinas de treino
+            testTable: [], // cards rodando na nossa API, que permitam passar parametros
             useTable: [], // cards com a cloudfunction
             downloadTable: [], // cards função de baixar modelo
         }
