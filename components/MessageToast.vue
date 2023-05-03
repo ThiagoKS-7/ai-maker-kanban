10e103fa2cc0a5b428fc8ae939ca46a9b6bfc09e
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-05-03 20:41:22 -0300

    Começa a implementar toast

---
+++ components/MessageToast.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-if="showToast" class="toast">
+        {{ message }}
+    </div>
+</template>
+<script>
+export default {
+    name: 'MessageToast',
+    props: {
+        showToast: {type:Boolean, required: true, default:false},
+        message: {type:String, required: true, default: 'Erro! Contate o admin do sistema'},
+        className: {type:String, required: false, default:''},
+    }
+}
+</script>
